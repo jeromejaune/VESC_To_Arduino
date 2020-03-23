b3d5e3a54dce9db2a49ddaa76b79f5b8b3954d2c
jeromejaune <32591578+jeromejaune@users.noreply.github.com>
2020-03-23 10:19:10 +0100

    Adding files

---
+++ VESC_To_Arduino.ino
@@ -0,0 +1,61 @@
+/*
+  Name: VESC_To_Arduino.ino
+  Autor : Roze Jerome
+
+  Note:
+ Not all pins on the Mega and Mega 2560 support change interrupts,
+ so only the following can be used for RX:
+ 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
+
+ Not all pins on the Leonardo and Micro support change interrupts,
+ so only the following can be used for RX:
+ 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+*/
+
+#include <VescUart.h>
+#include<SoftwareSerial.h>
+
+#define RxD 2 /** pin 2 (connecter au TX du VESC) !!! A modifier si vous changez les pins de connection au VESC !!! */
+#define TxD 3 /** pin 3 (connecter au RX du VESC) !!! A modifier si vous changez les pins de connection au VESC !!! */
+
+/** Initiate VescUart class */
+VescUart UART;
+
+SoftwareSerial SerialVESC(RxD, TxD);
+
+void setup() {
+
+  /** Connexion à l'USB du PC */
+  Serial.begin(9600);
+
+  /** Connexion au VESC !!! A modifier en fonction de la vitesse de transfert de votre VESC */         
+  SerialVESC.begin(115200);
+  
+  while (!SerialVESC) {;}
+
+  /** Define which ports to use as UART */
+  UART.setSerialPort(&SerialVESC);
+}
+
+void loop() {
+  
+  /** Call the function getVescValues() to acquire data from VESC */
+  if ( UART.getVescValues() ) {
+
+    SerialVESC.print("Vitesse du moteur : ");
+    SerialVESC.println(UART.data.rpm);
+    SerialVESC.print("Voltage : ");
+    SerialVESC.println(UART.data.inpVoltage);
+    SerialVESC.print("Courant : ");
+    SerialVESC.println(UART.data.ampHours);
+    SerialVESC.print("tachometerAbs : ");
+    SerialVESC.println(UART.data.tachometerAbs);
+
+  }
+  else
+  {
+    SerialVESC.println("Connexion échouée");
+  }
+
+  delay(50);
+}
